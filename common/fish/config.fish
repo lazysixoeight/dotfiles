720424a6fbacdcf619ab65fdb8f99fa23067f641
lazysixoeight <eaverage48@gmail.com>
2025-04-26 12:01:33 +0700

    More Neovim

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -1,3 +1,4 @@
+functions --erase fish_command_not_found
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- common/nvim/flake.nix
+++ common/nvim/flake.nix
@@ -62,7 +62,6 @@
     # this allows you to use ${pkgs.system} whenever you want in those sections
     # without fear.
 
-    # see :help nixCats.flake.outputs.overlays
     dependencyOverlays = /* (import ./overlays inputs) ++ */ [
       # This overlay grabs all the inputs named in the format
       # `plugins-<pluginName>`
@@ -92,23 +91,89 @@
       # this section is for dependencies that should be available
       # at RUN TIME for plugins. Will be available to PATH within neovim terminal
       # this includes LSPs
-      lspsAndRuntimeDeps = {
-        general = with pkgs; [
+      lspsAndRuntimeDeps = with pkgs; {
+        general = [
+          universal-ctags
+          ripgrep
+          fd
+          stdenv.cc.cc
+          nix-doc
+          lua-language-server
+          nixd
+          stylua
+        ];
+        kickstart-debug = [
+          delve
+        ];
+        kickstart-lint = [
+          markdownlint-cli
         ];
       };
 
       # This is for plugins that will load at startup without using packadd:
-      startupPlugins = {
-        gitPlugins = with pkgs.neovimPlugins; [ ];
-        general = with pkgs.vimPlugins; [ ];
+      startupPlugins = with pkgs.vimPlugins; {
+        general = [
+          vim-sleuth
+          lazy-nvim
+          comment-nvim
+          gitsigns-nvim
+          which-key-nvim
+          telescope-nvim
+          telescope-fzf-native-nvim
+          telescope-ui-select-nvim
+          nvim-web-devicons
+          plenary-nvim
+          nvim-lspconfig
+          lazydev-nvim
+          fidget-nvim
+          conform-nvim
+          nvim-cmp
+          luasnip
+          cmp_luasnip
+          cmp-nvim-lsp
+          cmp-path
+          monokai-pro-nvim
+          todo-comments-nvim
+          mini-nvim
+          nvim-treesitter.withAllGrammars
+          indent-blankline-nvim
+          # This is for if you only want some of the grammars
+          # (nvim-treesitter.withPlugins (
+          #   plugins: with plugins; [
+          #     nix
+          #     lua
+          #   ]
+          # ))
+        ];
+        kickstart-debug = [
+          nvim-dap
+          nvim-dap-ui
+          nvim-dap-go
+          nvim-nio
+        ];
+        kickstart-lint = [
+          nvim-lint
+        ];
+        kickstart-autopairs = [
+          nvim-autopairs
+        ];
+        kickstart-neo-tree = [
+          neo-tree-nvim
+          nui-nvim
+          # nixCats will filter out duplicate packages
+          # so you can put dependencies with stuff even if they're
+          # also somewhere else
+          nvim-web-devicons
+          plenary-nvim
+        ];
       };
 
       # not loaded automatically at startup.
       # use with packadd and an autocommand in config to achieve lazy loading
-      optionalPlugins = {
-        gitPlugins = with pkgs.neovimPlugins; [ ];
-        general = with pkgs.vimPlugins; [ ];
-      };
+      # NOTE: this template is using lazy.nvim so, which list you put them in is irrelevant.
+      # startupPlugins or optionalPlugins, it doesnt matter, lazy.nvim does the loading.
+      # I just put them all in startupPlugins. I could have put them all in here instead.
+      optionalPlugins = {};
 
       # shared libraries to be added to LD_LIBRARY_PATH
       # variable available to nvim runtime
@@ -164,7 +229,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      nvim = {pkgs , name, ... }: {
+      nvim = { pkgs, name, ... }: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -175,6 +240,8 @@
           # your alias may not conflict with your other packages.
           aliases = [ "vim" ];
           # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+          hosts.python3.enable = true;
+          hosts.node.enable = true;
         };
         # and a set of categories that you want
         # (and other information to pass to lua)
@@ -183,6 +250,21 @@
           gitPlugins = true;
           customPlugins = true;
           test = true;
+
+          kickstart-autopairs = true;
+          kickstart-neo-tree = true;
+          kickstart-debug = true;
+          kickstart-lint = true;
+          kickstart-indent_line = true;
+
+          # this kickstart extra didnt require any extra plugins
+          # so it doesnt have a category above.
+          # but we can still send the info from nix to lua that we want it!
+          kickstart-gitsigns = true;
+
+          # we can pass whatever we want actually.
+          have_nerd_font = false;
+
           example = {
             youCan = "add more than just booleans";
             toThisSet = [
@@ -190,6 +272,7 @@
               "will be accessible to your lua with"
               "nixCats('path.to.value')"
               "see :help nixCats"
+              "and type :NixCats to see the categories set in nvim"
             ];
           };
         };
@@ -260,5 +343,4 @@
     inherit utils nixosModule homeModule;
     inherit (utils) templates;
   });
-
 }
